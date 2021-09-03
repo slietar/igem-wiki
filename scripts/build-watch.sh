9ac494959fc7c0ae56d65b84b4992c54a8ab571c
Simon Liétar <simon@lietar.net>
2021-09-03 14:52:01 +0200

    Add description

--- scripts/build-watch.sh
+++ scripts/build-watch.sh
@@ -1,8 +1,3 @@
-mkdir -p dist\
-  && npx sass src/styles/index.scss:dist/styles.css --watch\
+npx sass src/styles/index.scss:dist/styles.css --watch\
   & npx pug src/pages --out dist --silent --watch\
-  & npx cpx 'src/assets/**/*' dist/assets --watch\
-  & npx cpx 'node_modules/@fontsource/inter/files/inter-latin-variable-wghtOnly-normal.woff2' dist/assets/fonts --watch\
-  & npx cpx 'node_modules/@fontsource/merriweather/files/merriweather-latin-700-normal.woff2' dist/assets/fonts --watch\
-  & npx cpx 'node_modules/@fontsource/secular-one/files/secular-one-latin-400-normal.woff2' dist/assets/fonts --watch\
   & npx parcel watch src/scripts/index.js --cache-dir tmp/parcel --out-file dist/scripts.js
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 mkdir -p dist\
   && npx sass src/styles/index.scss:dist/styles.css --no-source-map\
-  && npx pug src/pages --out dist --silent\
+  && npx pug src/pages --out tmp/pages1 --silent\
+  && npx posthtml tmp/pages1/* --config posthtml.json --output dist > /dev/null\
   && npx cpx 'src/assets/**/*' dist/assets\
   && npx cpx 'node_modules/@fontsource/inter/files/inter-latin-variable-wghtOnly-normal.woff2' dist/assets/fonts\
   && npx cpx 'node_modules/@fontsource/merriweather/files/merriweather-latin-700-normal.woff2' dist/assets/fonts\
