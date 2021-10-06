cf6f21327ae27c22e61e6c2206c5704bcef56175
Simon Liétar <simon@lietar.net>
2021-10-06 14:25:35 +0200

    Improve and fix many pages

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,9 @@ mkdir -p dist\
   && npx pug src/pages --out tmp/pages1 --silent\
   && npx posthtml tmp/pages1/* --config posthtml.json --output dist > /dev/null\
   && npx cpx 'src/assets/**/*' dist/assets\
-  && npx cpx 'node_modules/@fontsource/inter/files/inter-latin-variable-wghtOnly-normal.woff2' dist/assets/fonts\
-  && npx cpx 'node_modules/@fontsource/merriweather/files/merriweather-latin-700-normal.woff2' dist/assets/fonts\
-  && npx cpx 'node_modules/@fontsource/secular-one/files/secular-one-latin-400-normal.woff2' dist/assets/fonts\
+  && npx cpx 'node_modules/@fontsource/inter/files/inter-latin-400-normal.woff' dist/assets/fonts\
+  && npx cpx 'node_modules/@fontsource/inter/files/inter-latin-500-normal.woff' dist/assets/fonts\
+  && npx cpx 'node_modules/@fontsource/inter/files/inter-latin-600-normal.woff' dist/assets/fonts\
+  && npx cpx 'node_modules/@fontsource/merriweather/files/merriweather-latin-700-normal.woff' dist/assets/fonts\
+  && npx cpx 'node_modules/@fontsource/secular-one/files/secular-one-latin-400-normal.woff' dist/assets/fonts\
   && npx parcel build src/scripts/index.js --cache-dir tmp/parcel --log-level 2 --no-source-maps --out-file dist/scripts.js
