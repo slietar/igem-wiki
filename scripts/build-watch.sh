ce092b4dd96851f6bd185e1f9af41c1be909a83a
Simon Liétar <simon@lietar.net>
2021-10-13 16:10:27 +0200

    Add html beautification

--- scripts/build-watch.sh
+++ scripts/build-watch.sh
@@ -1,3 +1,4 @@
 npx sass src/styles/index.scss:dist/styles.css --watch\
   & npx pug src/pages --out dist --silent --watch\
+  & npx cpx 'src/assets/**/*' dist/assets --watch\
   & npx parcel watch src/scripts/index.js --cache-dir tmp/parcel --out-file dist/scripts.js
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@ mkdir -p dist\
   && npx sass src/styles/index.scss:dist/styles.css --no-source-map\
   && npx pug src/pages --out tmp/pages1 --silent\
   && npx posthtml tmp/pages1/* --config posthtml.json --output dist > /dev/null\
+  && find dist/*.html -type f -exec npx js-beautify --file {} --replace --type html --quiet \;\
   && npx cpx 'src/assets/**/*' dist/assets\
   && npx cpx 'node_modules/@fontsource/merriweather/files/merriweather-latin-700-normal.woff' dist/assets/fonts\
   && npx parcel build src/scripts/index.js --cache-dir tmp/parcel --log-level 2 --no-source-maps --out-file dist/scripts.js
